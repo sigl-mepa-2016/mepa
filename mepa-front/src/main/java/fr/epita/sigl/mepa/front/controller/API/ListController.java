33edd44454616a956ce6711e381e52917b34b831
lucas boisserie <lucas.boisserie@gmail.com>
2015-07-11 15:43:17 +0200

    Create an Object compose of list in place

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/API/ListController.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/API/ListController.java
@@ -1,24 +1,29 @@
 package fr.epita.sigl.mepa.front.controller.API;
 
-import fr.epita.sigl.mepa.front.APIpojo.ClassicObject;
+import fr.epita.sigl.mepa.front.APIpojo.ListSimpleObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 public class ListController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DefaultController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ListController.class);
 
-    @RequestMapping
-    public List<ClassicObject> list()
+    @RequestMapping("/api/list")
+    public ListSimpleObject list()
     {
-        List<ClassicObject> retunList = new ArrayList<>();
-        //
-        return retunList;
+        ListSimpleObject items = new ListSimpleObject();
+
+        items.addSimpleObject("1", "toto");
+        items.addSimpleObject("2", "toto");
+        items.addSimpleObject("3", "toto");
+        items.addSimpleObject("4", "toto");
+        items.addSimpleObject("5", "toto");
+        items.addSimpleObject("6", "toto");
+        items.addSimpleObject("7", "toto");
+
+        return items;
     }
 }
