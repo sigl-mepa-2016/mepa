ef335ce0eda9244edc9ca92cc24872578ab8e34d
lucas boisserie <lucas.boisserie@gmail.com>
2015-07-18 21:32:27 +0200

    clean file

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/API/DefaultController.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/API/DefaultController.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-//Replace RestController by controler when web page welcome and help are done
-//TODO: faire 2 pages web pour welcome et help
 @Controller
 @RequestMapping("/api")
 public class DefaultController {
