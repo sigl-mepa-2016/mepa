707354250cf6bed5f223eef47f2f6740c7d08561
lucas boisserie <lucas.boisserie@gmail.com>
2015-07-18 21:31:42 +0200

    Add welcome page

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/API/DefaultController.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/API/DefaultController.java
@@ -2,12 +2,13 @@ package fr.epita.sigl.mepa.front.controller.API;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //Replace RestController by controler when web page welcome and help are done
 //TODO: faire 2 pages web pour welcome et help
-@RestController
+@Controller
 @RequestMapping("/api")
 public class DefaultController {
 
