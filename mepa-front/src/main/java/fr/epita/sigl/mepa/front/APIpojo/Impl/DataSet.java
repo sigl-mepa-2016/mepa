4cb3dce21b0345e9214480659597349884d6b101
lucas boisserie <lucas.boisserie@gmail.com>
2015-07-21 00:39:16 +0200

    remove xml annotation

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/APIpojo/Impl/DataSet.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/APIpojo/Impl/DataSet.java
@@ -2,13 +2,9 @@ package fr.epita.sigl.mepa.front.APIpojo.Impl;
 
 import fr.epita.sigl.mepa.front.APIpojo.Pojo;
 
-import javax.xml.bind.annotation.XmlElement;
 import java.util.Date;
 import java.util.Map;
 
-/**
- * Created by Calu on 20/07/2015.
- */
 public class DataSet implements Pojo {
     private String _id;
     private String name;
@@ -25,32 +21,26 @@ public class DataSet implements Pojo {
         this.lastModified = lastModified;
     }
 
-    @XmlElement
     public String get_id() {
         return _id;
     }
 
-    @XmlElement
     public String getName() {
         return name;
     }
 
-    @XmlElement
     public String getOwner() {
         return owner;
     }
 
-    @XmlElement
     public String getTheme() {
         return theme;
     }
 
-    @XmlElement
     public Date getLastModified() {
         return lastModified;
     }
 
-    @XmlElement
     public Map<String, String> getFieldMap() {
         return fieldMap;
     }
