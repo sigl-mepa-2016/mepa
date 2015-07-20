c2cc24906fe1eb46192633065fe22e2d9219a67f
lucas boisserie <lucas.boisserie@gmail.com>
2015-07-21 00:42:57 +0200

    fix xml convertion

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/APIpojo/Impl/DataSet.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/APIpojo/Impl/DataSet.java
@@ -2,9 +2,12 @@ package fr.epita.sigl.mepa.front.APIpojo.Impl;
 
 import fr.epita.sigl.mepa.front.APIpojo.Pojo;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 import java.util.Map;
 
+@XmlRootElement
 public class DataSet implements Pojo {
     private String _id;
     private String name;
@@ -12,6 +15,9 @@ public class DataSet implements Pojo {
     private String theme;
     private Date lastModified;
     private Map<String, String> fieldMap;
+
+    public DataSet() {
+    }
 
     public DataSet(String _id, String name, String owner, String theme, Date lastModified) {
         this._id = _id;
@@ -21,26 +27,32 @@ public class DataSet implements Pojo {
         this.lastModified = lastModified;
     }
 
+    @XmlElement
     public String get_id() {
         return _id;
     }
 
+    @XmlElement
     public String getName() {
         return name;
     }
 
+    @XmlElement
     public String getOwner() {
         return owner;
     }
 
+    @XmlElement
     public String getTheme() {
         return theme;
     }
 
+    @XmlElement
     public Date getLastModified() {
         return lastModified;
     }
 
+    @XmlElement
     public Map<String, String> getFieldMap() {
         return fieldMap;
     }
