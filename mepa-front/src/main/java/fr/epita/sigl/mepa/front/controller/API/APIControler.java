eebd5c8bc32fbc87c2ce068e6da9595d44c41235
lucas boisserie <lucas.boisserie@gmail.com>
2015-07-20 10:58:13 +0200

    dataController

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/API/APIControler.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/API/APIControler.java
@@ -8,7 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -70,7 +73,15 @@ public class APIControler {
      * @return
      */
     @RequestMapping("/dataSet/{dataSetID}/data")
-    public Object detailsDataSet(@PathVariable Long dataSetID) {
+    public Object detailsDataSet(@PathVariable Long dataSetID, @RequestParam Map<String,String> allRequestParams) {
+        if (allRequestParams.isEmpty())
+        {
+
+        }
+        else
+        {
+
+        }
         return "datasetID = " + dataSetID;
     }
 }
