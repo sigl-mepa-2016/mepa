1bc5c90058d173ac323afdd771d91408b9736b16
lucas boisserie <lucas.boisserie@gmail.com>
2015-07-21 23:44:31 +0200

    remove authors

--- mepa-core/src/main/java/fr/epita/sigl/mepa/core/dao/DataDao.java
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/dao/DataDao.java
@@ -2,9 +2,6 @@ package fr.epita.sigl.mepa.core.dao;
 
 import fr.epita.sigl.mepa.core.domain.Data;
 
-/**
- * Created by Calu on 21/07/2015.
- */
 public interface DataDao {
 
     void create(Data data);
