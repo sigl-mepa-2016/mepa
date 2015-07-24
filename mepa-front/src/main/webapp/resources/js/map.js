e9b65f40790a8133483fa75a5c1b47e8cf92e94f
Valentin BAUDRY <vb.valentinbaudry@gmail.com>
2015-07-25 01:44:42 +0200

    grey map bug fixed when clicking on map tab

--- mepa-front/src/main/webapp/resources/js/map.js
+++ mepa-front/src/main/webapp/resources/js/map.js
@@ -1,23 +1,25 @@
-var map;
-var infowindow;
-var content;
-var latitude;
-var longitude;
-var pointsToDisplay = [];
-var googleMapPoints = [];
-var markers = [];
+var map = null;
+var infowindow = null;
+var content = null;
 var center = new google.maps.LatLng(48.8532, 2.3499); //Centre de paris (Notre-Dame de Paris)
+var pointsToDisplay = [];
+var markers = [];
+var datasetId = null;
+var size = null;
 
 
-function initialize() {
+function initialize()
+{
+    $("#map-canvas").remove();
+    $("#carto-view").append("<div id='map-canvas' class='map-canvas'></div>");
 
-    var datasetId = document.getElementById("data").value;
-    var size = document.getElementById("size").value;
-    var dataJson = null;
+    datasetId = document.getElementById("data").value;
+    size = document.getElementById("size").value;
+
+    clearMarkers();
 
     var mapOptions = {
-        zoom: 10,
-        center: center
+        zoom: 12
     };
 
     map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -33,17 +35,39 @@ function initialize() {
 
             var pos = new google.maps.LatLng(position.coords.latitude,
                 position.coords.longitude);
+
+            map.setCenter(pos);
             content = 'You are here !';
 
             pointsToDisplay.push({pos : pos, info : content, isGeoLoc : true});
 
+            drawMultipleMarkersOnMap();
+
         }, function() {
             handleNoGeolocation(true);
+            drawMultipleMarkersOnMap();
         });
     } else {
         // Browser doesn't support Geolocation
         handleNoGeolocation(false);
+        drawMultipleMarkersOnMap();
     }
+}
+
+function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+        content = 'Error: The Geolocation service failed.';
+    } else {
+        content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    map.setCenter(center);
+    pointsToDisplay.push({pos : center, info : content, isGeoLoc : true});
+}
+
+function drawMultipleMarkersOnMap() {
+
+    var dataJson = null;
 
     $.ajax({
         dataType: "json",
@@ -62,8 +86,8 @@ function initialize() {
             parseFloat(dataJson.data.longitude[i])), info : info, isGeoLoc : false});
     }
 
-    for (var i = 0; i < googleMapPoints.length; i++) {
-        addMarker(googleMapPoints[i].pos, pointsToDisplay[i].info, pointsToDisplay[i].isGeoLoc);
+    for (var j = 0; j < pointsToDisplay.length; j++) {
+        addMarker(pointsToDisplay[j].pos, pointsToDisplay[j].info, pointsToDisplay[j].isGeoLoc);
     }
 
     var mc = new MarkerClusterer(map, markers);
@@ -71,17 +95,6 @@ function initialize() {
     google.maps.event.addListener(map, 'click', function() {
         infowindow.close(map, this);
     });
-}
-
-
-function handleNoGeolocation(errorFlag) {
-    if (errorFlag) {
-        content = 'Error: The Geolocation service failed.';
-    } else {
-        content = 'Error: Your browser doesn\'t support geolocation.';
-    }
-
-    pointsToDisplay.push({pos : center, info : content, isGeoLoc : true});
 }
 
 
@@ -104,7 +117,6 @@ function addMarker(position, content, isGeoLoc) {
             icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
         });
     }
-
     google.maps.event.addListener(marker, 'click', function () {
         infowindow.setContent(content);
         infowindow.open(map, this);
@@ -122,4 +134,13 @@ function clearMarkers() {
 }
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+function isCartTabActive()
+{
+    if ($('#carto-tab').attr('class') == 'active')
+        initialize();
+
+    else
+        setTimeout(isCartTabActive, 500);
+}
+
+google.maps.event.addDomListener(window, 'load', isCartTabActive);
