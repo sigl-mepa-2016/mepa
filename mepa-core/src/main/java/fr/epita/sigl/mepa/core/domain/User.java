b16e8b4b40619e4ab9ed74168dc27ef85e571fc5
lucas boisserie <lucas.boisserie@gmail.com>
2015-07-25 02:28:35 +0200

    add User model

---
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/User.java
@@ -0,0 +1,44 @@
+package fr.epita.sigl.mepa.core.domain;
+
+import org.jongo.marshall.jackson.oid.MongoObjectId;
+
+
+public class User {
+
+    @MongoObjectId
+    private String _id;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
