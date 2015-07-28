95b715ec370fbe7b1d1b7246300a6b92068f5439
Nosykaa <bes.emeline93@hotmail.fr>
2015-07-28 16:41:16 +0200

    forget one file

---
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/model/search/SortForm.java
@@ -0,0 +1,16 @@
+package fr.epita.sigl.mepa.front.model.search;
+
+/**
+ * Created by emeline on 27/07/2015.
+ */
+public class SortForm {
+    private String sort;
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+}
