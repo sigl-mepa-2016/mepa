95903f3bcb0f5804a43a7c3c5f9339f169c161c8
lucas boisserie <lucas.boisserie@gmail.com>
2015-07-29 16:27:02 +0200

    fix create user

--- mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/User.java
+++ mepa-core/src/main/java/fr/epita/sigl/mepa/core/domain/User.java
@@ -18,7 +18,7 @@ public class User {
         this.password = password;
     }
 
-    public User(String name, String password) {
+    public  User(String name, String password) {
         this.name = name;
         this.password = password;
     }
