7d1b8692083eef6f36b75bf11cf42a89c0b3799b
lucas boisserie <lucas.boisserie@gmail.com>
2015-07-29 14:47:20 +0200

    add graph fonction

--- mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/API/GraphController.java
+++ mepa-front/src/main/java/fr/epita/sigl/mepa/front/controller/API/GraphController.java
@@ -6,6 +6,7 @@ import fr.epita.sigl.mepa.front.APIpojo.Impl.ErrorMessage;
 import fr.epita.sigl.mepa.front.APIpojo.Impl.GraphInput;
 import fr.epita.sigl.mepa.front.APIpojo.Impl.SuccessMessage;
 import fr.epita.sigl.mepa.front.APIpojo.Pojo;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,23 +20,34 @@ public class GraphController {
     @Autowired
     private GraphService graphService;
 
-    @RequestMapping(value = "/{DataSetID}", method = RequestMethod.GET)
-    public Graph getGraphConf(@PathVariable String DataSetId)
-    {
-        return this.graphService.getById(DataSetId);
+    @RequestMapping(value = "/get/{dataSetID}", method = RequestMethod.GET)
+    public Graph getGraphConf(@PathVariable String dataSetID) {
+        return this.graphService.getById(dataSetID  );
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public Pojo setGraphConf(@RequestBody GraphInput graphInput, @RequestHeader(value = "Authorization", defaultValue = "") String authorization)
-    {
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public Pojo setGraphConf(@RequestBody GraphInput graphInput, @RequestHeader(value = "Authorization", defaultValue = "") String authorization) {
         Pojo resultAuthorization = new UserController().checkToken(authorization);
         if (resultAuthorization instanceof ErrorMessage)
             return resultAuthorization;
         LOG.debug("", graphInput);
 
-        this.graphService.create(new Graph(graphInput.getId(), graphInput.getGrapheColor1(), graphInput.getGrapheColor2(), graphInput.getGrapheJson()));
+        this.graphService.create(new Graph(new ObjectId(graphInput.getId()), graphInput.getGrapheType(), graphInput.getGrapheColor1(), graphInput.getGrapheColor2(), graphInput.getGrapheJson()));
 
         return new SuccessMessage("success add");
+
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public Pojo updateGraphConf(@RequestBody GraphInput graphInput, @RequestHeader(value = "Authorization", defaultValue = "") String authorization) {
+        Pojo resultAuthorization = new UserController().checkToken(authorization);
+        if (resultAuthorization instanceof ErrorMessage)
+            return resultAuthorization;
+        LOG.debug("", graphInput);
+
+        this.graphService.update(new Graph(new ObjectId(graphInput.getId()), graphInput.getGrapheType(), graphInput.getGrapheColor1(), graphInput.getGrapheColor2(), graphInput.getGrapheJson()));
+
+        return new SuccessMessage("success update");
 
     }
 }
